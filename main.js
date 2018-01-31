1375b3310b24cdb97da157b04c18022b8b247d5e
Deyka Howe <deyka.howe@gmail.com>
2018-01-31 15:40:25 -0800

    create new files and update coding

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const
     app = express(),
     logger = require('morgan'), // this is the logs request to the terminal 
     mongoose = require('mongoose'),
-    Recipe = require('./model/Recipe.js')
+    Recipe = require('./model/Recipe.js'),
+    path = require('path'),
+    bodyParser = require('body-parser')
 
 // connect to Mongo 
 mongoose.connect('mongodb://localhost/recipesdb', function(err) {
@@ -13,7 +15,19 @@ mongoose.connect('mongodb://localhost/recipesdb', function(err) {
 // Middleware
 app.use(logger('dev'))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // routes
+app.get('/', function(req, res){
+    res.render('index')
+})
 app.get('/recipes', function(req, res) {
     Recipe.find({}, function(err, recipes) {
         res.json(recipes)
@@ -28,6 +42,10 @@ app.get('/recipes/:id', function(req, res) {
         }   else {}
         res.json(recipe)
      })              // res.send("Profile page for recipe: " + req.params.id)
+})
+
+app.post('/recipes', function(req, res) {
+    Recipe.create(req.body)
 })
 
 // listen to requests on PORT 3000
